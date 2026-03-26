1894ad8df1d2355372597bd1317cab19e63d495c
giorochi <rochiragiovanni87@gmail.com>
2026-03-26 17:17:43 +0100

    Create config.js with bot configuration settings
    Add configuration file with token, prefix, and roles.

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  TOKEN: "MTQ4NTY0MTE2NDUwNDY5NDkwNQ.GsbMyb.B2CLNwjnLyHEWMnQwLb0hkvLzJwsaGRiWoG9Ao",
+  PREFIX: "!",
+  ROLES: {
+    "Sommo Pontefice": 5,
+    "Cardinale": 4,
+    "Vescovo": 3,
+    "Sacerdote": 2,
+    "Diacono": 1,
+    "Cittadino": 0
+  }
+};
