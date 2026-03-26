b5f45775ac09a5dc8a207dc2283d2b8d139ee49a
giorochi <rochiragiovanni87@gmail.com>
2026-03-26 17:40:35 +0100

    Refactor user management functions in economy.js

--- economy.js
+++ economy.js
@@ -1,23 +1,19 @@
 const db = require('./db');
 
-function getUser(id) {
+function getUser(id, name) {
   return new Promise((resolve) => {
     db.get("SELECT * FROM players WHERE id = ?", [id], (err, row) => {
       if (!row) {
-        db.run("INSERT INTO players (id, name) VALUES (?, ?)", [id, "User"]);
-        return resolve(null);
+        db.run("INSERT INTO players (id, name) VALUES (?, ?)", [id, name]);
+        return resolve({ id, name, money: 100 });
       }
       resolve(row);
     });
   });
 }
 
-function updateMoney(id, amount) {
+function addMoney(id, amount) {
   db.run("UPDATE players SET money = money + ? WHERE id = ?", [amount, id]);
 }
 
-function setMoney(id, amount) {
-  db.run("UPDATE players SET money = ? WHERE id = ?", [amount, id]);
-}
-
-module.exports = { getUser, updateMoney, setMoney };
+module.exports = { getUser, addMoney };
