63ce50bd504de6d93b18f345fc88544ed4ae9cb5
giorochi <rochiragiovanni87@gmail.com>
2026-03-27 18:09:54 +0100

    Change command registration to use guild-specific route

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,7 +12,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.CLIENT_ID);
 async function deploy() {
   try {
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
       { body: commands }
     );
     console.log('Comandi registrati');
